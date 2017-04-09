2dd0dbb9ef3575dfdafb7150fcae1fe3d0892fdc
nick <nckcol@gmail.com>
2017-04-09 11:05:19 +0300

    refactor

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -12,23 +12,7 @@ import {Http} from "@angular/http";
 
 export class AppComponent {
 
-    link: string = "https://likeit-risingapp.herokuapp.com/";
-
     constructor(private http: Http) {
-    }
-
-    private apiSend() {
-
-        console.log(this.link);
-
-        this.http.get(this.link, { withCredentials: true })
-            // .map((res) => {JSON.parse(res.text())})
-            .subscribe(
-                (res) => {
-                    console.log(res);
-                },
-                err => console.error(err)
-            );
     }
 
     private logOut() {
